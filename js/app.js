4a0e16789f243ed7a362c05fe2d6bf94386cb503
Roberto <robert-art1@hotmail.com>
2019-11-20 16:49:15 -0600

    Primer commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).foundation()
+
+document.getElementById('holaaa').addEventListener('click',function(){
+    alert("hola")
+    var elem = document.querySelector('#ho');
+    ho.add.className = "active"
+})
+
+

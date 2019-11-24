99bfadb541ff040cc6c663205bdb6a9ae335bc83
Roberto <robert-art1@hotmail.com>
2019-11-24 09:18:21 -0600

    incorporación de gráficas

--- js/app.js
+++ js/app.js
@@ -1,3 +1,78 @@
 $(document).foundation()
 AOS.init();
 
+Highcharts.chart('container', {
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: 0,
+      plotShadow: false
+    },
+    title: {
+      text: 'Ventas<br>Noviembre<br>2019',
+      align: 'center',
+      verticalAlign: 'middle',
+      y: 60
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+      pie: {
+        dataLabels: {
+          enabled: true,
+          distance: -50,
+          style: {
+            fontWeight: 'bold',
+            color: 'white'
+          }
+        },
+        startAngle: -90,
+        endAngle: 90,
+        center: ['50%', '75%'],
+        size: '110%'
+      }
+    },
+    series: [{
+      type: 'pie',
+      name: 'Browser share',
+      innerSize: '50%',
+      data: [
+        ['Chrome', 58.9],
+        ['Firefox', 13.29],
+        ['Internet Explorer', 13],
+        ['Edge', 3.78],
+        ['Safari', 3.42],
+        {
+          name: 'Other',
+          y: 7.61,
+          dataLabels: {
+            enabled: false
+          }
+        }
+      ]
+    }]
+  });
+  
+  Highcharts.chart('container2', {
+    data: {
+      table: 'datatable'
+    },
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'Data extracted from a HTML table in the page'
+    },
+    yAxis: {
+      allowDecimals: false,
+      title: {
+        text: 'Units'
+      }
+    },
+    tooltip: {
+      formatter: function () {
+        return '<b>' + this.series.name + '</b><br/>' +
+          this.point.y + ' ' + this.point.name.toLowerCase();
+      }
+    }
+  });

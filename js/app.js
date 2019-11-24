023215cd2aa4e84f24377ad4579470b81a16ac56
Roberto <robert-art1@hotmail.com>
2019-11-24 08:58:47 -0600

    animaciones, preguntas y submetas

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
 $(document).foundation()
-
-document.getElementById('holaaa').addEventListener('click',function(){
-    alert("hola")
-    var elem = document.querySelector('#ho');
-    ho.add.className = "active"
-})
-
+AOS.init();
 
